be86810a1a805eafade33b4268764390df137f86
jacqueline-lam <jacquelinekarinlam@gmail.com>
2020-01-12 18:42:59 -0500

    Generate schema file

--- db/migrate/20200112232944_create_sessions.rb
+++ db/migrate/20200112232944_create_sessions.rb
@@ -1,8 +1,8 @@
 class CreateSessions < ActiveRecord::Migration
   def change
     create_table :sessions do |t|
-      t.string :user_id
       t.date :date
+      t.string :user_id
     end
   end
 end
--- db/migrate/20200112233002_create_problems.rb
+++ db/migrate/20200112233002_create_problems.rb
@@ -1,4 +1,10 @@
 class CreateProblems < ActiveRecord::Migration
   def change
+    create_table :problems do |t|
+      t.string :color
+      t.string :grade
+      t.string :image
+      t.string :session_id
+    end
   end
 end
