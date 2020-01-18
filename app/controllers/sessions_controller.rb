c0d0b1236134e7aff6c37cfab28ef5fb262e4b27
jacqueline-lam <jacquelinekarinlam@gmail.com>
2020-01-17 21:22:03 -0500

    Create controller action '/logout' to process GET request to /logout and clear session hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(username: params[:username], password: params[:password])
+    user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
@@ -17,4 +17,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  # Users can log out
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 end
