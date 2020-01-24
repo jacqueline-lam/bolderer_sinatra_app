f28a1af009797b588ffa5e5e02ed1143f61bb228
jacqueline-lam <jacquelinekarinlam@gmail.com>
2020-01-24 15:11:36 -0500

    Add comments

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 # Tells AR where database is located
-# establish connection using AR gem to our SQLite through database
+# establish connection (using AR gem) to our SQLite through database
 # sets name of db management system to hold date
 # sets path to app's database
 ActiveRecord::Base.establish_connection(
